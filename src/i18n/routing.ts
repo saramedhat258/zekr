0845f4154501694f8e870a699c4005636e8d58d4
sara <sara2003me@gmail.com>
2026-09-07 20:40:56 +0300

    set localeDetection: false to allow defalut language to be ar

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,5 +2,6 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
     locales: ["ar", "en"],
-    defaultLocale: "ar"
+    defaultLocale: "ar",
+    localeDetection: false
 });
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,10 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-export const proxy = createMiddleware(routing);
+const handleRequest = createMiddleware(routing);
+
+export default handleRequest;
+export const proxy = handleRequest;
 
 export const config = {
   matcher: ["/((?!api|_next|.*\\..*).*)"]
